4c99cbd9d2a6794362667a0f12c4561c487b3bd1
HaiderAin <haider.ain111@gmail.com>
2026-06-20 15:30:14 +0530

    joins

---
+++ Joins/1.INNER JOIN_example.sql
@@ -0,0 +1,5 @@
+--1.Display each employee’s full name along with their department name using an INNER JOIN.
+SELECT e.first_name ||' ' || e.last_name as full_name, d.department_name
+FROM employee e
+INNER JOIN department d
+ON e.employee_id =d.employee_id;
